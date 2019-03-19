98fdc361e8b8e71aa4970b647ad712b5a57d9f28
Chylli <chylli@163.com>
2019-03-20 00:28:00 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
 
-
 fn main() {
     match repl() {
         Err(err) => println!("Error: {}", err),
-        _ => ()
+        _ => (),
     }
 }
 
-fn repl() -> Result<(),String>{
+fn repl() -> Result<(), String> {
     println!("\nWelcome to the Rscheme!\n");
     let mut rl = Editor::<()>::new();
     let history = "/tmp/rscheme_history"; // TODO change it to ./.rscheme_history
-    if let Err(e) = rl.load_history(history){ // TODO handle error more elegently
+    if let Err(e) = rl.load_history(history) {
+        // TODO handle error more elegently
         println!("There a problem when loading {}: {}.", history, e);
     }
 
@@ -22,29 +22,22 @@ fn repl() -> Result<(),String>{
         match readline {
             Ok(line) => {
                 rl.add_history_entry(line.as_ref());
-                match line.as_str(){
+                match line.as_str() {
                     ".quit" => {
-                        println!("Bye!");
                         break;
-                    },
-                    _ =>  println!("{}", line)
+                    }
+                    _ => println!("{}", line),
                 }
-            },
-            Err(ReadlineError::Interrupted) => {
-                println!("Bye!");
-                break
-            },
-            Err(ReadlineError::Eof) => {
-                println!("Bye!");
-                break
-            },
+            }
+            Err(ReadlineError::Interrupted) => break,
+            Err(ReadlineError::Eof) => break,
             Err(err) => {
                 println!("Error: {:?}", err);
-                break
+                break;
             }
         }
     }
+    println!("Bye!");
     rl.save_history(history).unwrap();
     Ok(())
-
 }
